1b6f23419f971b116ddeaf83e525cce6e217786d
Micha Sengotta <micha.sengotta@gmail.com>
2023-05-22 22:15:18 +0200

    Add handle duplicates to find the best offers

--- cm_wizard/services/shopping_wizard_service.py
+++ cm_wizard/services/shopping_wizard_service.py
@@ -1,5 +1,6 @@
 import logging
 from dataclasses import dataclass
+from typing import Iterator, TypeVar
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
@@ -7,6 +8,8 @@ _logger.setLevel(logging.DEBUG)
 # max int32, however python3 has no such limit
 # other "infinities" like float("inf") or math.inf are of type float
 infinity = 2147483647
+
+purchase_type = tuple[int, str]
 
 
 @dataclass
@@ -16,31 +19,53 @@ class RequestSeller:
 
 
 @dataclass
-class ResultSeller:
-    id: str
-    offers: list[tuple[str, int]]
+class Result:
+    total_price: int
+    sellers: dict[str, list[tuple[str, int]]]
+
+
+T = TypeVar("T")
+
+
+def create_matrix(col_count: int, row_count: int, initial_value: T) -> list[list[T]]:
+    return [[initial_value] * col_count] * row_count
+
+
+def unique(lst: list[T]) -> Iterator[T]:
+    """
+    Returns an iterator with unique elements.
+    Preserves the order of elements (in contrast to a set).
+    """
+    used = set()
+    for e in lst:
+        if e not in used:
+            used.add(e)
+            yield e
 
 
 class ShoppingWizardService:
-    # TODO: handle duplicates correctly
     # TODO: handle shipping costs
     def find_best_offers(
         self, wanted_cards: list[str], sellers: list[RequestSeller]
-    ) -> list[ResultSeller]:
+    ) -> Result:
         """
-        Returns one of the best combinations of cards to buy from sellers
+        Returns (one of) the best combinations of cards to buy from sellers
         in order to buy all wanted_cards.
         The wanted_cards may contain duplicates.
         The seller offers for each card must be sorted ascendingly by price.
         """
 
-        offers_to_buy: dict[str, list[tuple[str, int]]] = {
+        result_sellers: dict[str, list[tuple[str, int]]] = {
             seller.id: [] for seller in sellers
         }
 
-        price_table: list[list[int]] = [[infinity] * len(sellers)] * len(wanted_cards)
-        seller_history_table: list[list[list[int]]] = [[[]] * len(sellers)] * len(
-            wanted_cards
+        purchase_history_type = list[purchase_type]
+
+        price_table: list[list[int]] = create_matrix(
+            len(sellers), len(wanted_cards), infinity
+        )
+        purchase_history_table: list[list[purchase_history_type]] = create_matrix(
+            len(sellers), len(wanted_cards), []
         )
 
         best_seller_index = -1
@@ -48,21 +73,26 @@ class ShoppingWizardService:
             previous_best_card_price = (
                 0 if card_index == 0 else price_table[card_index - 1][best_seller_index]
             )
-            seller_history = (
+            purchase_history: purchase_history_type = (
                 []
                 if card_index == 0
-                else seller_history_table[card_index - 1][best_seller_index]
+                else purchase_history_table[card_index - 1][best_seller_index]
             )
             best_card_price = infinity
             for seller_index, seller in enumerate(sellers):
                 if card_id not in seller.offers:
-                    continue
+                    continue  # seller does not offer the card
 
-                seller_offer = min(seller.offers[card_id])
+                purchase = (seller_index, card_id)
+                purchase_count = purchase_history.count(purchase)
+                if len(seller.offers[card_id]) <= purchase_count:
+                    continue  # seller does not offer the card often enough
+
+                seller_offer = seller.offers[card_id][purchase_count]
                 price = previous_best_card_price + seller_offer
                 price_table[card_index][seller_index] = price
-                seller_history_table[card_index][seller_index] = seller_history + [
-                    seller_index
+                purchase_history_table[card_index][seller_index] = purchase_history + [
+                    purchase
                 ]
 
                 if price < best_card_price:
@@ -71,15 +101,21 @@ class ShoppingWizardService:
 
         best_price = price_table[-1][best_seller_index]
         _logger.info(f"best total price: {best_price}")
-        seller_history = seller_history_table[-1][best_seller_index]
-        for card_index, card_id in enumerate(wanted_cards):
-            seller_index = seller_history[card_index]
-            seller = sellers[seller_index]
-            offers_to_buy[seller.id].append((card_id, seller.offers[card_id][0]))
 
-        return [
-            ResultSeller(id=key, offers=value) for key, value in offers_to_buy.items()
+        best_purchase_history: list[purchase_type] = purchase_history_table[-1][
+            best_seller_index
         ]
+        for seller_index, card_id in unique(best_purchase_history):
+            purchase = (seller_index, card_id)
+            count = best_purchase_history.count(purchase)
+            seller = sellers[seller_index]
+            for i in range(count):
+                result_sellers[seller.id].append((card_id, seller.offers[card_id][i]))
+
+        return Result(
+            total_price=best_price,
+            sellers=result_sellers,
+        )
 
 
 shopping_wizard_service = ShoppingWizardService()
--- tests/services/test_shopping_wizard_service.py
+++ tests/services/test_shopping_wizard_service.py
@@ -1,11 +1,11 @@
 from cm_wizard.services.shopping_wizard_service import (
     RequestSeller,
-    ResultSeller,
+    Result,
     shopping_wizard_service,
 )
 
 
-def test_find_best_offers():
+def test_find_best_offers_basic():
     wanted_cards = ["c1", "c2", "c3"]
     sellers = [
         RequestSeller(
@@ -35,8 +35,51 @@ def test_find_best_offers():
 
     result = shopping_wizard_service.find_best_offers(wanted_cards, sellers)
 
-    assert result == [
-        ResultSeller(id="s1", offers=[("c1", 1)]),
-        ResultSeller(id="s2", offers=[("c2", 1), ("c3", 1)]),
-        ResultSeller(id="s3", offers=[]),
+    assert result == Result(
+        total_price=3,
+        sellers={
+            "s1": [("c1", 1)],
+            "s2": [("c2", 1), ("c3", 1)],
+            "s3": [],
+        },
+    )
+
+
+def test_find_best_offers_duplicate_wants():
+    wanted_cards = ["c1", "c2", "c2", "c3", "c3", "c3", "c3"]
+    sellers = [
+        RequestSeller(
+            id="s1",
+            offers={
+                "c1": [1],
+                "c2": [2],
+                "c3": [3],
+            },
+        ),
+        RequestSeller(
+            id="s2",
+            offers={
+                "c1": [2],
+                "c2": [1],
+                "c3": [1],
+            },
+        ),
+        RequestSeller(
+            id="s3",
+            offers={
+                "c2": [2],
+                "c3": [1, 2],
+            },
+        ),
     ]
+
+    result = shopping_wizard_service.find_best_offers(wanted_cards, sellers)
+
+    assert result == Result(
+        total_price=11,
+        sellers={
+            "s1": [("c1", 1), ("c2", 2), ("c3", 3)],
+            "s2": [("c2", 1), ("c3", 1)],
+            "s3": [("c3", 1), ("c3", 2)],
+        },
+    )
